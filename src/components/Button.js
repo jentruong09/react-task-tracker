cfeec539600c33e64286c6963831a1a305de92eb
Jen Truong <jennifertruong09@gmail.com>
2023-10-18 14:23:23 -0400

    added place holder tasks and changed up components to add them

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,21 @@
-const Button = () => {
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
   return (
     <div>
-      <button className='btn'>Add</button>
+      <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
     </div>
   )
 }
+
+Button.defaultProps = {
+    color: 'steelblue',
+  }
+  
+  Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+  }
 
 export default Button
